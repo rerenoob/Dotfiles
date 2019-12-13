c3401257464931ed8f5dccf51b78694b3f9e10fa
rerenoob <dphamsw@gmail.com>
2019-12-13 07:38:05 -0600

    Added virtualfish

--- config.fish
+++ config.fish
@@ -45,7 +45,7 @@ set -g theme_use_abbreviated_branch_name yes
 # set -g theme_display_docker_machine no
 # set -g theme_display_k8s_context yes
 # set -g theme_display_hg yes
-set -g theme_display_virtualenv no
+set -g theme_display_virtualenv yes
 # set -g theme_display_ruby no
 # set -g theme_display_nvm yes
 # set -g theme_display_user ssh
@@ -57,15 +57,19 @@ set -g theme_title_display_process yes
 # set -g theme_title_display_path no
 set -g theme_title_display_user yes
 # set -g theme_title_use_abbreviated_path no
-set -g theme_date_format "+%a %H:%M"
+set -g theme_date_format "+%a/%d/%m %H:%M"
 set -g theme_avoid_ambiguous_glyphs yes
 set -g theme_powerline_fonts yes 
 set -g theme_nerd_fonts yes
 # set -g theme_show_exit_status yes
 # set -g theme_display_jobs_verbose yes
-set -g default_user your_normal_user
+# set -g default_user your_normal_user
 set -g theme_color_scheme gruvbox
 # set -g fish_prompt_pwd_dir_length 0
 # set -g theme_project_dir_length 1
 set -g theme_newline_cursor yes
 set -g theme_newline_prompt '☀️ '
+
+# virtualfish
+eval (python -m virtualfish)
+eval (python -m virtualfish compat_aliases)
