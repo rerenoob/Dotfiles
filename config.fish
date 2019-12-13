30b88100bf8433bfc1fec476c96a0ccade6dc061
rerenoob <dphamsw@gmail.com>
2019-12-12 22:10:07 -0600

    Changed bobthefish config

--- config.fish
+++ config.fish
@@ -55,7 +55,7 @@ set -g theme_display_date yes
 set -g theme_display_cmd_duration yes
 set -g theme_title_display_process yes
 # set -g theme_title_display_path no
-# set -g theme_title_display_user yes
+set -g theme_title_display_user yes
 # set -g theme_title_use_abbreviated_path no
 set -g theme_date_format "+%a %H:%M"
 set -g theme_avoid_ambiguous_glyphs yes
@@ -63,9 +63,9 @@ set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 # set -g theme_show_exit_status yes
 # set -g theme_display_jobs_verbose yes
-# set -g default_user your_normal_user
+set -g default_user your_normal_user
 set -g theme_color_scheme dark
 # set -g fish_prompt_pwd_dir_length 0
 # set -g theme_project_dir_length 1
 set -g theme_newline_cursor yes
-set -g theme_newline_prompt '☀️  '
+set -g theme_newline_prompt '☀️ '
