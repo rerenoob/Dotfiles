993ca2725b2d9413aac10eeec23fb3f82cf617a3
rerenoob <dphamsw@gmail.com>
2019-12-12 22:00:16 -0600

    Added options for bobthefish

--- config.fish
+++ config.fish
@@ -23,8 +23,6 @@ alias speedtest='curl -o /dev/null http://speedtest.wdc01.softlayer.com/download
 echo "Follow the rabbit"
 
 # Setting PATH for Python 3.7
-# The original version is saved in .bash_profile.pysave
-# PATH="/Library/Frameworks/Python.framework/Versions/3.7/bin:${PATH}"
 export PATH
 export CLICOLOR=1
 
@@ -32,3 +30,42 @@ export CLICOLOR=1
 export FZF_DEFAULT_OPTS='--height=70% --preview="cat {}" --preview-window=right:60%:wrap'
 export FZF_DEFAULT_COMMAND='rg --files'
 export FZF_CTRL_T_COMMAND='$FZF_DEFAULT_COMMAND'
+
+# bobthefish overrides
+set -g theme_display_git yes 
+set -g theme_display_git_dirty yes
+set -g theme_display_git_untracked no
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_stashed_verbose yes
+set -g theme_display_git_master_branch yes
+# set -g theme_git_worktree_support yes
+set -g theme_use_abbreviated_branch_name yes
+# set -g theme_display_vagrant yes
+# set -g theme_display_docker_machine no
+# set -g theme_display_k8s_context yes
+# set -g theme_display_hg yes
+set -g theme_display_virtualenv no
+# set -g theme_display_ruby no
+# set -g theme_display_nvm yes
+# set -g theme_display_user ssh
+# set -g theme_display_hostname ssh
+# set -g theme_display_vi no
+set -g theme_display_date yes
+set -g theme_display_cmd_duration yes
+set -g theme_title_display_process yes
+# set -g theme_title_display_path no
+# set -g theme_title_display_user yes
+# set -g theme_title_use_abbreviated_path no
+set -g theme_date_format "+%a %H:%M"
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_powerline_fonts yes 
+set -g theme_nerd_fonts yes
+# set -g theme_show_exit_status yes
+# set -g theme_display_jobs_verbose yes
+# set -g default_user your_normal_user
+set -g theme_color_scheme dark
+# set -g fish_prompt_pwd_dir_length 0
+# set -g theme_project_dir_length 1
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '☀️  '
