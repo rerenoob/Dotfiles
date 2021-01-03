a325b1a3e37310c70f16ed5e4bef2726d9c4722d
phamine <53153445+rerenoob@users.noreply.github.com>
2021-01-03 12:37:37 -0600

    Update quoteoftheday.py
    Add default quote when the request fails.

--- quoteoftheday.py
+++ quoteoftheday.py
@@ -5,7 +5,10 @@ url = "http://quotes.rest/qod.json?category=inspire"
 
 response = requests.request("GET", url)
 
-parser = response.json()
-quote = parser["contents"]["quotes"][0]["quote"]
-author = parser["contents"]["quotes"][0]["author"]
-print(quote, '-', author)
+if response.status_code == 200:
+    parser = response.json()
+    quote = parser["contents"]["quotes"][0]["quote"]
+    author = parser["contents"]["quotes"][0]["author"]
+    print(quote, '-', author)
+else:
+    print('If you love life, do not waste time, because time if what life is made of. - Bruce Lee')
